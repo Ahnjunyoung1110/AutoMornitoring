4905a08dace389e8c5dd604848517e798f949ebf
jyahn <jyahn2008@naver.com>
2025-09-08 14:20:22 +0900

    First commit

---
+++ src/main/java/AutoMornitoring/AutoMornitoring/AutoMornitoringApplication.java
@@ -0,0 +1,13 @@
+package AutoMornitoring.AutoMornitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMornitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMornitoringApplication.class, args);
+	}
+
+}
